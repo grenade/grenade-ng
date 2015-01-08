6e8e71fc9844076ea10f6f9ad977a584e6bd27ef
Thijssen, Robin <Robin.Thijssen@bis.org>
2015-01-08 11:20:08 +0100

    include data folder in build

--- Gruntfile.js
+++ Gruntfile.js
@@ -353,6 +353,11 @@ module.exports = function (grunt) {
           cwd: '.',
           src: 'bower_components/bootstrap-sass-official/assets/fonts/bootstrap/*',
           dest: '<%= yeoman.dist %>'
+        }, {
+          expand: true,
+          cwd: '<%= yeoman.app %>/data/cv',
+          dest: '<%= yeoman.dist %>/data/cv',
+          src: ['*.json', '*.doc']
         }]
       },
       styles: {
