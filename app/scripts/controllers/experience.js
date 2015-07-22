1a44832ee98eade723b4ac36fd3a5347f81f0421
Rob Thijssen <rthijssen@gmail.com>
2015-07-22 10:57:20 +0100

    grab data files from githubusercontent

--- app/scripts/controllers/experience.js
+++ app/scripts/controllers/experience.js
@@ -9,29 +9,30 @@
  */
 angular.module('grenadeApp')
   .controller('ExperienceCtrl', function ($scope, $http) {
+    var cv_url = 'https://raw.githubusercontent.com/grenade/cv/master/'
     var experienceFiles = [
-      'data/cv/mozilla.json',
-      'data/cv/bis.json',
-      'data/cv/ihs.json',
-      'data/cv/beazley.json',
-      'data/cv/maersk.json',
-      'data/cv/rwe.json',
-      'data/cv/keane.json',
-      'data/cv/visa.json',
-      'data/cv/charteris.json',
-      'data/cv/bt-london.json',
-      'data/cv/hpa.json',
-      'data/cv/atos.json',
-      'data/cv/conchango.json',
-      'data/cv/mcorp.json',
-      'data/cv/bt-exeter.json',
-      'data/cv/centrax.json',
-      'data/cv/devon.json',
-      'data/cv/txtnation.json'
+      'mozilla.json',
+      'bis.json',
+      'ihs.json',
+      'beazley.json',
+      'maersk.json',
+      'rwe.json',
+      'keane.json',
+      'visa.json',
+      'charteris.json',
+      'bt-london.json',
+      'hpa.json',
+      'atos.json',
+      'conchango.json',
+      'mcorp.json',
+      'bt-exeter.json',
+      'centrax.json',
+      'devon.json',
+      'txtnation.json'
     ];
     var experiences = [];
     for (var i = experienceFiles.length - 1; i >= 0; i--) {
-      $http.get(experienceFiles[i]).success(function(data) {
+      $http.get(cv_url + experienceFiles[i]).success(function(data) {
         experiences.push(data);
         if (experiences.length === experienceFiles.length) {
           experiences.sort(function(a,b) { return b.period.start.localeCompare(a.period.start); } );
